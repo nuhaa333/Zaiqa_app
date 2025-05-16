7e2be034b90168d4e2ba641b6e977a52ba14256b
nuhaa333 <nuhaaaisha592@gmail.com>
2025-05-16 19:41:48 +0530

    again mismatch

--- Backend/auth.js
+++ Backend/auth.js
@@ -5,10 +5,15 @@ console.log('CLIENT_SECRET:', process.env.CLIENT_SECRET);
 console.log('redirect URI:',`${process.env.BACKEND_URL}/auth/google/callback`);
 const GoogleStrategy = require('passport-google-oauth20').Strategy;
 
+const callbackURL = process.env.NODE_ENV === 'production'
+  ? 'https://zaiqa-app-1.onrender.com/auth/google/callback'
+  : 'http://localhost:5000/auth/google/callback';
+
+
 passport.use(new GoogleStrategy({
   clientID: process.env.CLIENT_ID,
   clientSecret: process.env.CLIENT_SECRET,
-  callbackURL: `${process.env.BACKEND_URL}/auth/google/callback`
+  callbackURL: callbackURL
 },
 (accessToken, refreshToken, profile, done) => {
   // Here you can store the profile to your DB
