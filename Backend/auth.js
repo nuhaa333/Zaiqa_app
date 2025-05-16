5e2a9949bb6db5b6635e7a21ca187d6ff24c1262
nuhaa333 <nuhaaaisha592@gmail.com>
2025-05-16 18:40:25 +0530

    google auth changes

--- Backend/auth.js
+++ Backend/auth.js
@@ -1,12 +1,13 @@
 const passport = require('passport');
 console.log('CLIENT_ID:', process.env.CLIENT_ID);
 console.log('CLIENT_SECRET:', process.env.CLIENT_SECRET);
+console.log('redirect URI:',`${process.env.BACKEND_URL}/auth/google/callback`);
 const GoogleStrategy = require('passport-google-oauth20').Strategy;
 
 passport.use(new GoogleStrategy({
   clientID: process.env.CLIENT_ID,
   clientSecret: process.env.CLIENT_SECRET,
-  callbackURL: '/auth/google/callback'
+  callbackURL: `${process.env.BACKEND_URL}/auth/google/callback`
 },
 (accessToken, refreshToken, profile, done) => {
   // Here you can store the profile to your DB
